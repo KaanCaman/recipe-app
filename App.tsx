964b5da708c3163b2dc4596b306b0b6ee86a5a5f
KaanCaman <106434723+KaanCaman@users.noreply.github.com>
2025-06-17 17:38:31 +0300

    Add status bar component.

--- App.tsx
+++ App.tsx
@@ -3,11 +3,13 @@ import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
 import {Provider} from 'react-redux';
 import {store} from './src/redux/store';
+import DynamicStatusBar from './src/components/DynamicStatusBar/DynamicStatusBar';
 
 const App = () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
+        <DynamicStatusBar />
         <AppNavigator />
       </NavigationContainer>
     </Provider>
